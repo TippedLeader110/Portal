77b378bff2e2b0451c912130fd67fc5302c9aeff
TippedLeader110 <39187403+TippedLeader110@users.noreply.github.com>
2019-05-31 22:38:20 +0700

    0.0.2

---
+++ application/views/page/ajax/idsek.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="<?php echo base_url('plugins/ckeditor/ckeditor.js') ?>"></script>
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+	    <div class="modal-content">
+		    <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal">&times;</button>
+		        <h4 class="modal-title">Edit Data</h4>
+		    </div>
+		    <div class="modal-body">
+		        <textarea id="sekid" name="sekid"></textarea>
+		    </div>
+		    <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		    </div>
+		</div>
+  	</div>
+</div>
--- application/views/page/cidsekolah.php
+++ application/views/page/cidsekolah.php
@@ -1,8 +1,16 @@
+<script type="text/javascript" src="<?php echo base_url('plugins/ckeditor/ckeditor.js') ?>"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>source/css/customNav.css">
 <?php foreach ($sek as $key => $val): ?>
 <?php endforeach ?>
-<div class="container-fluid">
+<div class="container-fluid" >
 	<div class="row">
+		<div class="col-12">
+			<div class="head">
+				Dashboard Admin / Edit Tampilan Website / Pengaturan Identitas Sekolah
+			</div>
+		</div>
+	</div>
+	<div class="row" style="margin-top: 20px;">
 		<div class="col-3 ">
 			<div class="container-fluid" > 
 				<div class="row">
@@ -16,6 +24,9 @@
 						</div>
 					</div>
 				</div>
+				<div class="row">
+					<button>Edit </button>
+				</div>
 			</div>
 		</div>
 		<!-- <div class="col-1">
@@ -23,14 +34,23 @@
 		</div> -->
 		<div class="col-9 ">
 			<!-- // <?php $this->load->view('nav/cidnav') ?> -->
+			<hr>
+			<table>
+				<tr>
+					<th colspan="3" align="left"><h2 id="seknama" style="font-weight: normal;"></h2></th><th><i class="fas fa-edit"></th>
+				</tr>
+			</table>
+				<hr>
 			<table>
 				<tr>
 					<td id="tdd1" align="center"><a onclick="getVisi();" href="javascript:void(0);" class="nava"><h5 >Visi Sekolah</h5></a></td>
-					<td id="tdd2" align="center"><a onclick="getMisi();" href="javascript:void(0);" class="nava"><h5 >Visi Sekolah</h5></a></td>
-					<td id="tdd3" align="center"><a onclick="getSejarah();" href="javascript:void(0);" class="nava"><h5 >Visi Sekolah</h5></a></td>
+					<td id="tdd2" align="center"><a onclick="getMisi();" href="javascript:void(0);" class="nava"><h5 >Misi Sekolah</h5></a></td>
+					<td id="tdd3" align="center"><a onclick="getSejarah();" href="javascript:void(0);" class="nava"><h5 >Sejarah Sekolah</h5></a></td>
 				</tr>
 			</table>
 			<hr>
+			
+
 			<div id="sek">
 				
 			</div>
@@ -38,13 +58,49 @@
 	</div>
 </div>
 
+
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-xl">
+	    <div class="modal-content">
+		    <div class="modal-header">
+		    	<h4 class="modal-title">Edit Data</h4>
+		        <button type="button" class="close" data-dismiss="modal">&times;</button>
+		    </div>
+		    <div class="modal-body">
+		        <textarea id="sekid" name="sekid">
+		        <div id="sek1">
+				
+				</div>
+				</textarea>
+		    </div>
+		    <div class="modal-footer">
+		        <div id="but"></div><button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+		    </div>
+		</div>
+  	</div>
+</div>
+
 <script type="text/javascript">
+	 window.onload = function() {
+        CKEDITOR.replace('editor1');
+    };
+
 	$(document).ready(function(){
     // alert('yeah');
+    
     getSek();
     visi();
     // $("#sek").load('<?php echo base_url('Custom/ewarna')?>');
 	});
+
+	function modallol(ab){
+		var htm;
+		htm = "";
+	}
+
+	function al(){
+		
+	}
 
 	function getSek(){
 		var ab = 'visi';
@@ -64,10 +120,17 @@
            		var visi = result[0].visi;
            		var sejarah = result[0].sejarah;
            		var misi = result[0].misi;
+           		var nama = result[0].nama_sekolah;
+           		$('#editor1').val('visi');
+           		visi = '<button class="tombol" style="background: #335C81;color: white" data-toggle="modal" data-target="#myModal" onclick="mod();">Edit Visi</button><br>' + visi;
+           		var tombol = '<button type="button" onclick="al();" class="btn btn-primary" data-dismiss="modal">Simpan</button>'
+                $("#but").html(tombol);
+                $("#seknama").html(nama);
                 $("#sek").html(visi);
                 $("#tdd1").toggleClass("acttd", true);
                 $("#tdd2").toggleClass("acttd", false);
                 $("#tdd3").toggleClass("acttd", false);
+
            }
         });
 	}
@@ -89,7 +152,11 @@
            		var visi = result[0].visi;
            		var sejarah = result[0].sejarah;
            		var misi = result[0].misi;
+           		$("#sek1").html(visi);
+                visi = '<button class="tombol" style="background: #335C81;color: white" data-toggle="modal" data-target="#myModal" onclick="mod();">Edit Visi</button><br>' + visi;
+                var tombol = '<button type="button" onclick="SaveVisi();" class="btn btn-primary" data-dismiss="modal">Simpan</button>'
                 $("#sek").html(visi);
+                $("#but").html(tombol);
                 $("#tdd1").toggleClass("acttd", true);
                 $("#tdd2").toggleClass("acttd", false);
                 $("#tdd3").toggleClass("acttd", false);
@@ -115,7 +182,11 @@
            		var visi = result[0].visi;
            		var sejarah = result[0].sejarah;
            		var misi = result[0].misi;
+           		$("#sek1").html(misi);
+           		misi = '<button class="tombol" style="background: #335C81;color: white" data-toggle="modal" data-target="#myModal" onclick="mod();">Edit Misi</button><br>' + misi;
                 $("#sek").html(misi);
+                var tombol = '<button type="button" onclick="SaveMisi();" class="btn btn-primary" data-dismiss="modal">Simpan</button>'
+                $("#but").html(tombol);
                 $("#tdd1").toggleClass("acttd", false);
                 $("#tdd2").toggleClass("acttd", true);
                 $("#tdd3").toggleClass("acttd", false);
@@ -141,11 +212,18 @@
            		var visi = result[0].visi;
            		var sejarah = result[0].sejarah;
            		var misi = result[0].misi;
+           		$("#sek1").html(sejarah);
+           		sejarah = '<button class="tombol" style="background: #335C81;color: white" data-toggle="modal" data-target="#myModal" onclick="mod();">Edit Sejarah</button><br>' + sejarah;
                 $("#sek").html(sejarah);
+                var tombol = '<button type="button" onclick="SaveSejarah();" class="btn btn-primary" data-dismiss="modal">Simpan</button>'
+                $("#but").html(tombol);
                 $("#tdd1").toggleClass("acttd", false);
                 $("#tdd2").toggleClass("acttd", false);
                 $("#tdd3").toggleClass("acttd", true);
            }
         });
+	}
+	function mod(){
+
 	}
 </script>
