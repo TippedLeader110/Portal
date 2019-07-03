89eb1d32772b71b5f7e160cacc0bb055f41acbdd
rizalfahmi1 <39107426+rizalfahmi1@users.noreply.github.com>
2019-07-03 20:45:37 +0700

    log

---
+++ log_siswa.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2019 at 03:44 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sisko2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_siswa`
+--
+
+CREATE TABLE `log_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama_siswa` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` text NOT NULL,
+  `waktu` date NOT NULL,
+  `status` enum('ubah','hapus') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
