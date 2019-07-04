920afa7b3677b1dd8635901ae3f8fd0af97f428b
TippedLeader110 <39187403+TippedLeader110@users.noreply.github.com>
2019-07-04 12:21:44 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/TippedLeader110/Portal""
    This reverts commit 186c56efa83172491ea4140fe90a21add21b7aa3.

--- application/views/page/ckweb.php
+++ application/views/page/ckweb.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jj" style="border-radius: 0px;">
 	<center>
-		<h1 style="color: white">Template Warna</h1>
-		<h2 style="color: white">Semua template warna yang telah</h2>
-		<h2 style="color: white">disimpan admin</h2>
+		<h1 style="color: white">Kelola Data Website</h1>
+		<h2 style="color: white">Admin :</h2>
+		<h2 style="color: white">Pilih menu <i class="fas fa-bars"></i> Untuk Mengelola Data Web</h2>
 	</center>
 </div>
